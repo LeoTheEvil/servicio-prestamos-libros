ef694ac79eb2867c3083c335238e0245bc1baaab
LeoTheEvil <iceninjashidomaru@gmail.com>
2025-02-05 10:57:33 -0300

    Commit

--- src/main/java/org/acme/Modelos/Libro.java
+++ src/main/java/org/acme/Modelos/Libro.java
@@ -14,4 +14,29 @@ public class Libro {
     private String author;
     @Column(nullable=false)
     private String genre;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
